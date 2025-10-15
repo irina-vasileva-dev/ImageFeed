096f4cf6d31e28ce6f95742073a02a4639cb8fa4
Irina Vasileva <alex.grin15@yandex.ru>
2025-10-15 23:11:25 +0300

    Добавлен и свёрстан по макетам экран ленты изображений

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var imageCell: UIImageView!
+    
+    @IBOutlet var likeButton: UIButton!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+
+}
