4ac43017272eb1922f97490df10a3d50dbf77413
Irina Vasileva <alex.grin15@yandex.ru>
2025-11-13 20:50:17 +0300

    Правка кнопки Поделиться

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -37,10 +37,16 @@ final class SingleImageViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction private func didTapShareButton() {
+    @IBAction private func didTapShareButton(_ sender: UIButton) {
         guard let image = image else { return }
-        let activityItems: [Any] = [image]
-        let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        
+        let compressedImage = compressImageIfNeeded(image)
+        
+        let activityViewController = UIActivityViewController(
+            activityItems: [compressedImage],
+            applicationActivities: nil
+        )
+      
         present(activityViewController, animated: true, completion: nil)
     }
     
@@ -75,8 +81,14 @@ final class SingleImageViewController: UIViewController {
         let scrollViewSize = scrollView.bounds.size
         let contentSize = scrollView.contentSize
         
-        let horizontalInset = max((scrollViewSize.width - contentSize.width) * 0.5, 0)
-        let verticalInset = max((scrollViewSize.height - contentSize.height) * 0.5, 0)
+        let horizontalInset = max(
+            (scrollViewSize.width - contentSize.width) * 0.5,
+            0
+        )
+        let verticalInset = max(
+            (scrollViewSize.height - contentSize.height) * 0.5,
+            0
+        )
         
         scrollView.contentInset = UIEdgeInsets(
             top: verticalInset,
@@ -84,6 +96,30 @@ final class SingleImageViewController: UIViewController {
             bottom: verticalInset,
             right: horizontalInset
         )
+    }
+    
+    private func compressImageIfNeeded(_ image: UIImage) -> UIImage {
+        let maxFileSize: Int = 10 * 1024 * 1024
+        
+        guard let imageData = image.jpegData(compressionQuality: 1.0),
+              imageData.count > maxFileSize else {
+            return image
+        }
+        
+        var compression: CGFloat = 0.9
+        while let compressedData = image.jpegData(
+            compressionQuality: compression
+        ),
+              compressedData.count > maxFileSize && compression > 0.1 {
+            compression -= 0.1
+        }
+        
+        if let finalData = image.jpegData(compressionQuality: compression),
+           let compressedImage = UIImage(data: finalData) {
+            return compressedImage
+        }
+        
+        return image
     }
 }
 
